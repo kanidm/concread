62f44dacc3667fdf9639ff1552a598988432c750
Jake Martin <ajanimember@gmail.com>
2019-05-27 22:49:04 +1000

    added check_min_max and sort functions for SparseLeaf and implemented tests, probably need to implement more tests to make sure they both work 100%

--- src/collections/maple_tree/mod.rs
+++ src/collections/maple_tree/mod.rs
@@ -78,7 +78,7 @@ struct Node<K, V> {
 
 impl<K, V> SparseLeaf<K, V>
 where
-    K: PartialEq + Debug,
+    K: Clone + PartialEq + PartialOrd + Debug,
     V: Debug,
 {
     pub fn new() -> Self {
@@ -106,6 +106,9 @@ where
         }
     }
 
+    // insert takes a key and a value and tries to find a place to
+    // insert it, if the sparseLeaf is already full and therefore
+    // the key and value can't be inserted None is returned
     pub fn insert(&mut self, k: K, v: V) -> Option<()> {
         // Just find a free slot and insert
         for i in 0..CAPACITY {
@@ -122,9 +125,13 @@ where
             }
         }
 
-        return None;
+        None
     }
 
+    // search tries to find a key that matches k within the sparseLeaf
+    // if the key can't be found None is returned
+    // if the key is found but the value associated to it is a None value
+    // since this is not allowed we panick
     pub fn search(&mut self, k: &K) -> Option<&V> {
         for i in 0..CAPACITY {
             match &self.key[i] {
@@ -134,9 +141,7 @@ where
                             M::Some(v) => {
                                 return Some(v);
                             }
-                            M::None => {
-                                return None;
-                            }
+                            M::None => panic!("SparseLeaf - search failure. None value found associated to a valid key, aborting."),
                         }
                     }
                 }
@@ -148,6 +153,10 @@ where
         None
     }
 
+    // update attempts to change the associated value of a key with a new value
+    // if the key isn't found then None is returned and nothing is updated,
+    // if the value that was associated with the key before the update was a None
+    //  value then we panick because that should not happen
     pub fn update(&mut self, k: K, v: V) -> Option<V> {
         for i in 0..CAPACITY {
             match &self.key[i] {
@@ -168,13 +177,17 @@ where
 
             match nv {
                 M::Some(v) => return Some(v),
-                M::None => return None,
+                M::None => panic!("SparseLeaf - update failure. None value found associated to a valid key, aborting."),
             }
         }
 
         None
     }
 
+    // remove attempts to delete a key/value pair from the sparseLeaf
+    // if the key is found and the value isn't None then the value is returned
+    // if the key isn't found then None is returned
+    // if the keys associated value is None then we panic because that shouldn't happen
     pub fn remove(&mut self, k: &K) -> Option<V> {
         for i in 0..CAPACITY {
             println!("remove: {:?}", self.key[i]);
@@ -200,18 +213,90 @@ where
                 M::Some(v) => {
                     return Some(v);
                 }
-                M::None => {}
+                M::None => panic!(
+                    "SparseLeaf - remove() None value found associated to a valid key, aborting."
+                ),
             }
         }
+
         None
     }
 
+    // This function is used to help verify the validity of the entire tree
+    // this function returns true if all keys within the SparseLeaf are within the bounds
+    // min to max or equal to min or max or the SparseLeaf node is empty
+    // otherwise this returns false
+    pub fn check_min_max(&mut self, min: &K, max: &K) -> bool {
+        for i in 0..CAPACITY {
+            match &self.key[i] {
+                M::Some(v) => {
+                    if v >= min && v <= max {
+                        continue;
+                    } else {
+                        return false;
+                    }
+                }
+                M::None => {
+                    continue;
+                }
+            }
+        }
+
+        return true;
+    }
+
     // We need to sort *just before* we split if required.
+    // This function implements selection sort for a SparseLeaf
+    // creates a new SparseLeaf struct, inserts the minimal values
+    // one by one then overwrites the old struct with the new one
+    //
+    // we create the new node so we don't have to deal with the None values
+    // being in-between values otherwise the code would be more complex to handle
+    // compacting the values and then sorting or vice versa so there is no gaps
+    // between actual keys in the underlying array
+    pub fn sort(&mut self) {
+        let mut smallest_key_index: usize;
+        let mut sl: SparseLeaf<K, V> = SparseLeaf::new();
+        let mut sl_index: usize = 0;
+
+        // run once for every key in the sparseLeaf
+        for current_index in 0..CAPACITY {
+            smallest_key_index = 0;
+
+            // run a pass over the remaining items to be sorted to find the
+            // entry with the smallest key and swap it for the item at currentIndex
+            for i in 0..8 {
+                match self.key[i] {
+                    M::Some(_) => {
+                        if self.key[i] < self.key[smallest_key_index] {
+                            smallest_key_index = i;
+                        }
+                    }
+                    M::None => continue,
+                }
+            }
+
+            // swap the found element into the new SparseLeaf with the M::None
+            // that is already in the SparseLeaf instead of just using the insert method
+            // on the new SparseLeaf so the sorting function will keep working
+            //
+            // we could also just just insert the values into the new node and set the value of
+            // the old node to M::None manually but that would require more code and I figured
+            // this was a bit cleaner, thoughts?
+            mem::swap(&mut self.key[smallest_key_index], &mut sl.key[sl_index]);
+            mem::swap(&mut self.value[smallest_key_index], &mut sl.value[sl_index]);
+            sl_index += 1;
+        }
+
+        *self = sl;
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::SparseLeaf;
+    use super::M;
+    use collections::maple_tree::CAPACITY;
 
     #[test]
     fn test_sparse_leaf_search() {
@@ -224,6 +309,16 @@ mod tests {
 
         // test invalid search
         assert!(sl.search(&3).is_none());
+
+        sl = SparseLeaf::new();
+
+        for i in 0..CAPACITY {
+            sl.insert(i, i);
+        }
+
+        sl.remove(&3);
+
+        assert!(sl.search(&4).is_some());
     }
 
     #[test]
@@ -255,9 +350,8 @@ mod tests {
 
         // test insert after node is already full
 
-        for i in 0..7 {
-            // i+1 because 0 was already used as a key
-            sl.insert(i + 1, i + 1);
+        for i in 1..CAPACITY {
+            sl.insert(i, i);
         }
 
         assert!(sl.insert(8, 8).is_none())
@@ -273,5 +367,95 @@ mod tests {
         // check removing a value that exists
         sl.insert(0, 0);
         assert!(sl.remove(&0).is_some());
+
+        // check removing existing values out of order is successfull
+        let remove_keys = [3, 7, 8, 1, 4];
+        for i in 0..CAPACITY {
+            sl.insert(i, i);
+        }
+        for i in 0..remove_keys.len() {
+            assert!(sl.remove(&i).is_some());
+        }
+    }
+
+    #[test]
+    fn test_sparse_leaf_check_min_max() {
+        let mut sl: SparseLeaf<usize, usize> = SparseLeaf::new();
+
+        // test that check_min_max returns true when the sparseLeaf is empty
+        assert!(sl.check_min_max(&0, &8));
+
+        // insert 8 values from 0 - 7
+        for i in 0..CAPACITY - 3 {
+            sl.insert(i, i);
+        }
+
+        assert!(sl.check_min_max(&0, &8));
+
+        // test that check_min_max returns some when the values are out of the range
+        // and returns the first value that is found outside the range.
+
+        sl.insert(10, 10);
+        sl.insert(11, 11);
+        sl.insert(12, 12);
+        assert!(sl.check_min_max(&0, &8) == false);
+    }
+
+    #[test]
+    fn test_sparse_leaf_sort() {
+        // test sorting full node
+        let mut sl: SparseLeaf<usize, usize> = SparseLeaf::new();
+        let mut test_vals: [usize; 8] = [3, 8, 7, 4, 2, 1, 5, 6];
+        let mut sorted_test_vals: [usize; 8] = [1, 2, 3, 4, 5, 6, 7, 8];
+
+        for val in test_vals.iter() {
+            sl.insert(*val, *val);
+        }
+        for i in 0..CAPACITY {
+            match sl.key[i] {
+                M::Some(v) => {
+                    println!("{0}", v);
+                }
+                M::None => println!("None"),
+            }
+        }
+        sl.sort();
+
+        for i in 0..CAPACITY {
+            // the code inside match is usefull for debuging if a test fails
+            match sl.key[i] {
+                M::Some(v) => {
+                    println!(
+                        "(actualValue = {0}) - (sortedTestValue = {1})",
+                        v, sorted_test_vals[i]
+                    );
+                }
+                M::None => println!("None - {}", sorted_test_vals[i]),
+            }
+            assert!(sl.key[i] == M::Some(sorted_test_vals[i]));
+        }
+
+        // test sorting half full node with M::None's inbetween each value
+        // i.e [3, None, 4, None, 2, None, 1, None]
+
+        test_vals = [3, 8, 4, 6, 2, 7, 1, 5];
+        let none_positions: [usize; 4] = [8, 6, 7, 5];
+        sl = SparseLeaf::new();
+
+        for val in test_vals.iter() {
+            sl.insert(*val, *val);
+        }
+
+        // remove every second value from sl
+        for val in none_positions.iter() {
+            sl.remove(&val);
+        }
+
+        sl.sort();
+
+        for i in 0..4 {
+            println!("{} <-> ", sorted_test_vals[i]);
+            assert!(sl.key[i] == M::Some(sorted_test_vals[i]));
+        }
     }
 }
