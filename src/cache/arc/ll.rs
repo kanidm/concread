52f9c6d5c496d1d4277dd47851b5535082a62b15
William Brown <william@blackhats.net.au>
2020-05-13 21:03:07 +1000

    Fix ll to use sigil nodes

--- src/cache/arc/ll.rs
+++ src/cache/arc/ll.rs
@@ -1,6 +1,7 @@
 use std::fmt::Debug;
 use std::hash::Hash;
 use std::marker::PhantomData;
+use std::mem::MaybeUninit;
 use std::ptr;
 
 #[derive(Clone, Debug)]
@@ -13,12 +14,12 @@ where
     size: usize,
 }
 
-#[derive(Clone, Debug)]
+#[derive(Debug)]
 pub(crate) struct LLNode<K>
 where
     K: Hash + Eq + Ord + Clone + Debug,
 {
-    pub(crate) k: K,
+    pub(crate) k: MaybeUninit<K>,
     next: *mut LLNode<K>,
     prev: *mut LLNode<K>,
 }
@@ -29,6 +30,7 @@ where
     K: Hash + Eq + Ord + Clone + Debug,
 {
     next: *mut LLNode<K>,
+    end: *mut LLNode<K>,
     phantom: PhantomData<&'a K>,
 }
 
@@ -37,16 +39,18 @@ where
     K: Hash + Eq + Ord + Clone + Debug,
 {
     pub(crate) fn new() -> Self {
+        let (head, tail) = LLNode::create_markers();
         LL {
-            head: ptr::null_mut(),
-            tail: ptr::null_mut(),
+            head,
+            tail,
             size: 0,
         }
     }
 
     pub(crate) fn iter_mut(&self) -> LLIterMut<K> {
         LLIterMut {
-            next: self.head,
+            next: unsafe { (*self.head).next },
+            end: self.tail,
             phantom: PhantomData,
         }
     }
@@ -60,23 +64,17 @@ where
 
     // Append an arbitrary node into this set.
     pub(crate) fn append_n(&mut self, n: *mut LLNode<K>) {
-        if self.tail.is_null() {
-            // If tail is null, head must also be null!
-            debug_assert!(self.head.is_null());
-            self.head = n;
-            self.tail = n;
-        } else {
-            // If tail is set, head must NOT be null!
-            debug_assert!(!self.head.is_null());
-            unsafe {
-                // And the tail's next must be null.
-                debug_assert!((*self.tail).next.is_null());
-            }
-            unsafe {
-                (*n).prev = self.tail;
-                (*self.tail).next = n;
-                self.tail = n;
-            }
+        // Who is to the left of tail?
+        unsafe {
+            debug_assert!((*self.tail).next.is_null());
+            debug_assert!(!(*self.tail).prev.is_null());
+            let pred = (*self.tail).prev;
+            debug_assert!(!pred.is_null());
+            debug_assert!((*pred).next == self.tail);
+            (*n).prev = pred;
+            (*n).next = self.tail;
+            (*pred).next = n;
+            (*self.tail).prev = n;
         }
         self.size += 1;
     }
@@ -84,7 +82,7 @@ where
     // Given a node ptr, extract and put it at the tail. IE hit.
     pub(crate) fn touch(&mut self, n: *mut LLNode<K>) {
         debug_assert!(self.size > 0);
-        if n == self.tail {
+        if n == unsafe { (*self.tail).prev } {
             // Done, no-op
         } else {
             let _ = self.extract(n);
@@ -94,28 +92,38 @@ where
 
     // remove this node from the ll, and return it's ptr.
     pub(crate) fn pop(&mut self) -> *mut LLNode<K> {
-        let n = self.head;
+        let n = unsafe { (*self.head).next };
         debug_assert!(!n.is_null());
         self.extract(n);
         n
     }
 
     #[cfg(test)]
-    pub(crate) fn peek_head(&self) -> Option<&LLNode<K>> {
-        if self.head.is_null() {
+    pub(crate) fn peek_head(&self) -> Option<&K> {
+        debug_assert!(!self.head.is_null());
+        let next = unsafe { (*self.head).next };
+        if next == self.tail {
             None
         } else {
-            let l = unsafe { &(*self.head) as &LLNode<K> };
+            let l = unsafe {
+                let ptr = (*next).k.as_ptr();
+                &(*ptr) as &K
+            };
             Some(l)
         }
     }
 
     #[cfg(test)]
-    pub(crate) fn peek_tail(&self) -> Option<&LLNode<K>> {
-        if self.tail.is_null() {
+    pub(crate) fn peek_tail(&self) -> Option<&K> {
+        debug_assert!(!self.tail.is_null());
+        let prev = unsafe { (*self.tail).prev };
+        if prev == self.head {
             None
         } else {
-            let l = unsafe { &(*self.tail) as &LLNode<K> };
+            let l = unsafe {
+                let ptr = (*prev).k.as_ptr();
+                &(*ptr) as &K
+            };
             Some(l)
         }
     }
@@ -123,37 +131,13 @@ where
     // Cut a node out from this list from any location.
     pub(crate) fn extract(&mut self, n: *mut LLNode<K>) {
         debug_assert!(!n.is_null());
-        if n == self.head && n == self.tail {
-            // must be the only node, set head and tail to null.
-            debug_assert!(self.size == 1);
-            self.tail = ptr::null_mut();
-            self.head = ptr::null_mut();
-        } else if n == self.head {
-            debug_assert!(self.size > 1);
-            unsafe {
-                self.head = (*n).next;
-                (*self.head).prev = ptr::null_mut();
-            }
-        } else if n == self.tail {
-            debug_assert!(self.size > 1);
-            unsafe {
-                self.tail = (*n).prev;
-                (*self.tail).next = ptr::null_mut();
-            }
-        } else {
-            unsafe {
-                let left = (*n).prev;
-                let right = (*n).next;
-                debug_assert!(!left.is_null());
-                debug_assert!(!right.is_null());
-                (*left).next = right;
-                (*right).prev = left;
-            }
-        }
-        // null the links in the node we are poping.
+
         unsafe {
-            (*n).next = ptr::null_mut();
-            (*n).prev = ptr::null_mut();
+            let prev = (*n).prev;
+            let next = (*n).next;
+            // prev <-> n <-> next
+            (*next).prev = prev;
+            (*prev).next = next;
         }
         self.size -= 1;
     }
@@ -181,18 +165,62 @@ impl<K> LLNode<K>
 where
     K: Hash + Eq + Ord + Clone + Debug,
 {
+    #[inline]
+    pub(crate) fn create_markers() -> (*mut Self, *mut Self) {
+        let head = Box::into_raw(Box::new(LLNode {
+            k: MaybeUninit::uninit(),
+            next: ptr::null_mut(),
+            prev: ptr::null_mut(),
+        }));
+        let tail = Box::into_raw(Box::new(LLNode {
+            k: MaybeUninit::uninit(),
+            next: ptr::null_mut(),
+            prev: head,
+        }));
+        unsafe {
+            (*head).next = tail;
+        }
+        (head, tail)
+    }
+
+    #[inline]
     pub(crate) fn new(k: K) -> *mut Self {
         let b = Box::new(LLNode {
-            k,
+            k: MaybeUninit::new(k),
             next: ptr::null_mut(),
             prev: ptr::null_mut(),
         });
         Box::into_raw(b)
     }
 
+    #[inline]
     pub(crate) fn free(v: *mut Self) {
         debug_assert!(!v.is_null());
         let _ = unsafe { Box::from_raw(v) };
+    }
+}
+
+impl<K> AsRef<K> for LLNode<K>
+where
+    K: Hash + Eq + Ord + Clone + Debug,
+{
+    fn as_ref(&self) -> &K {
+        unsafe {
+            let ptr = self.k.as_ptr();
+            &(*ptr) as &K
+        }
+    }
+}
+
+impl<K> AsMut<K> for LLNode<K>
+where
+    K: Hash + Eq + Ord + Clone + Debug,
+{
+    fn as_mut(&mut self) -> &mut K {
+        unsafe {
+            let ptr = self.k.as_mut_ptr();
+            &mut (*ptr) as &mut K
+        }
     }
 }
 
@@ -203,10 +231,11 @@ where
     type Item = &'a mut K;
 
     fn next(&mut self) -> Option<Self::Item> {
-        if self.next.is_null() {
+        debug_assert!(!self.next.is_null());
+        if self.next == self.end {
             None
         } else {
-            let r = Some(unsafe { &mut (*self.next).k });
+            let r = Some(unsafe { (*self.next).as_mut() });
             self.next = unsafe { (*self.next).next };
             r
         }
@@ -228,49 +257,49 @@ mod tests {
         let n4 = ll.append_k(4);
         // Check that n1 is the head, n3 is tail.
         assert!(ll.len() == 4);
-        assert!(ll.peek_head().unwrap().k == 1);
-        assert!(ll.peek_tail().unwrap().k == 4);
+        assert!(ll.peek_head().unwrap() == &1);
+        assert!(ll.peek_tail().unwrap() == &4);
 
         // Touch 2, it's now tail.
         ll.touch(n2);
         assert!(ll.len() == 4);
-        assert!(ll.peek_head().unwrap().k == 1);
-        assert!(ll.peek_tail().unwrap().k == 2);
+        assert!(ll.peek_head().unwrap() == &1);
+        assert!(ll.peek_tail().unwrap() == &2);
 
         // Touch 1 (head), it's the tail now.
         ll.touch(n1);
         assert!(ll.len() == 4);
-        assert!(ll.peek_head().unwrap().k == 3);
-        assert!(ll.peek_tail().unwrap().k == 1);
+        assert!(ll.peek_head().unwrap() == &3);
+        assert!(ll.peek_tail().unwrap() == &1);
 
         // Touch 1 (tail), it stays as tail.
         ll.touch(n1);
         assert!(ll.len() == 4);
-        assert!(ll.peek_head().unwrap().k == 3);
-        assert!(ll.peek_tail().unwrap().k == 1);
+        assert!(ll.peek_head().unwrap() == &3);
+        assert!(ll.peek_tail().unwrap() == &1);
 
         // pop from head
         let _n3 = ll.pop();
         assert!(ll.len() == 3);
-        assert!(ll.peek_head().unwrap().k == 4);
-        assert!(ll.peek_tail().unwrap().k == 1);
+        assert!(ll.peek_head().unwrap() == &4);
+        assert!(ll.peek_tail().unwrap() == &1);
 
         // cut a node out from any (head, mid, tail)
         let _n2 = ll.extract(n2);
         assert!(ll.len() == 2);
-        assert!(ll.peek_head().unwrap().k == 4);
-        assert!(ll.peek_tail().unwrap().k == 1);
+        assert!(ll.peek_head().unwrap() == &4);
+        assert!(ll.peek_tail().unwrap() == &1);
 
         let _n1 = ll.extract(n1);
         assert!(ll.len() == 1);
-        assert!(ll.peek_head().unwrap().k == 4);
-        assert!(ll.peek_tail().unwrap().k == 4);
+        assert!(ll.peek_head().unwrap() == &4);
+        assert!(ll.peek_tail().unwrap() == &4);
 
         // test touch on ll of size 1
         ll.touch(n4);
         assert!(ll.len() == 1);
-        assert!(ll.peek_head().unwrap().k == 4);
-        assert!(ll.peek_tail().unwrap().k == 4);
+        assert!(ll.peek_head().unwrap() == &4);
+        assert!(ll.peek_tail().unwrap() == &4);
         // Remove last
         let _n4 = ll.pop();
         assert!(ll.len() == 0);
