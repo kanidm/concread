096ca39c447041d404f6333ce7286dde0b445be4
William Brown <william@blackhats.net.au>
2021-09-19 10:40:43 +1000

    Add hit pct

--- benches/arccache.rs
+++ benches/arccache.rs
@@ -1,6 +1,4 @@
-use criterion::{
-    criterion_group, criterion_main, BenchmarkId, Criterion, SamplingMode, Throughput,
-};
+use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion, Throughput};
 use function_name::named;
 use rand::distributions::uniform::SampleUniform;
 use rand::{thread_rng, Rng};
@@ -8,9 +6,10 @@ use std::collections::HashMap;
 use std::fmt::Debug;
 use std::hash::Hash;
 use std::time::{Duration, Instant};
-use uuid::Uuid;
+// use uuid::Uuid;
 
 use concread::arcache::ARCache;
+use criterion::measurement::{Measurement, ValueFormatter};
 
 /*
  * A fixed dataset size, with various % of cache pressure (5, 10, 20, 40, 60, 80, 110)
@@ -23,20 +22,20 @@ use concread::arcache::ARCache;
  * As above but could we measure hit rate as well?
  *
  */
+#[derive(Debug)]
+struct DataPoint {
+    elapsed: Duration,
+    csize: usize,
+    hit_count: u32,
+    attempt: u32,
+    hit_pct: f64,
+}
 
 enum AccessPattern<T>
 where
     T: SampleUniform + PartialOrd + Clone,
 {
     Random(T, T),
-}
-
-#[derive(Debug)]
-struct DataPoint {
-    elapsed: Duration,
-    csize: usize,
-    hit_count: usize,
-    hit_pct: f64,
 }
 
 impl<T> AccessPattern<T>
@@ -47,9 +46,80 @@ where
         match self {
             AccessPattern::Random(min, max) => {
                 let mut rng = thread_rng();
-                rng.gen_range((min.clone()..max.clone()))
+                rng.gen_range(min.clone()..max.clone())
             }
         }
+    }
+}
+
+pub struct HitPercentageFormatter;
+
+impl ValueFormatter for HitPercentageFormatter {
+    fn format_value(&self, value: f64) -> String {
+        // eprintln!("⚠️  format_value -> {:?}", value);
+        format!("{}%", value)
+    }
+
+    fn format_throughput(&self, _throughput: &Throughput, value: f64) -> String {
+        // eprintln!("⚠️  format_throughput -> {:?}", value);
+        format!("{}%", value)
+    }
+
+    fn scale_values(&self, typical_value: f64, values: &mut [f64]) -> &'static str {
+        // eprintln!("⚠️  scale_values -> typ {:?} : {:?}", typical_value, values);
+        // panic!();
+        "%"
+    }
+
+    fn scale_throughputs(
+        &self,
+        _typical_value: f64,
+        _throughput: &Throughput,
+        values: &mut [f64],
+    ) -> &'static str {
+        // eprintln!("⚠️  scale_throughputs -> {:?}", values);
+        "%"
+    }
+
+    fn scale_for_machines(&self, values: &mut [f64]) -> &'static str {
+        // eprintln!("⚠️  scale_machines -> {:?}", values);
+        "%"
+    }
+}
+
+pub struct HitPercentage;
+
+impl Measurement for HitPercentage {
+    type Intermediate = (u32, u32);
+    type Value = (u32, u32);
+
+    fn start(&self) -> Self::Intermediate {
+        unreachable!("HitPercentage requires the use of iter_custom");
+    }
+
+    fn end(&self, i: Self::Intermediate) -> Self::Value {
+        // eprintln!("⚠️  end -> {:?}", i);
+        i
+    }
+
+    fn add(&self, v1: &Self::Value, v2: &Self::Value) -> Self::Value {
+        // eprintln!("⚠️  add -> {:?} + {:?}", v1, v2);
+        (v1.0 + v2.0, v1.1 + v2.1)
+    }
+
+    fn zero(&self) -> Self::Value {
+        // eprintln!("⚠️  zero -> (0,0)");
+        (0, 0)
+    }
+
+    fn to_f64(&self, val: &Self::Value) -> f64 {
+        let x = (f64::from(val.0) / f64::from(val.1)) * 100.0;
+        // eprintln!("⚠️  to_f64 -> {:?} -> {:?}", val, x);
+        x
+    }
+
+    fn formatter(&self) -> &dyn ValueFormatter {
+        &HitPercentageFormatter
     }
 }
 
@@ -87,8 +157,22 @@ where
 
     let mut elapsed = Duration::from_secs(0);
     let mut hit_count = 0;
+    let mut attempt = 0;
+
+    for _i in 0..warm_iters {
+        let k = access_pattern.next();
+        let v = backing_set.get(&k).cloned().unwrap();
+
+        let mut wr_txn = arc.write();
+        // hit/miss process.
+        if !wr_txn.contains_key(&k) {
+            wr_txn.insert(k, v);
+        }
+        wr_txn.commit();
+    }
 
     for _i in 0..iters {
+        attempt += 1;
         let k = access_pattern.next();
         let v = backing_set.get(&k).cloned().unwrap();
 
@@ -112,43 +196,87 @@ where
         elapsed,
         csize,
         hit_count,
+        attempt,
         hit_pct,
     }
 }
 
+macro_rules! basic_single_thread_x_small_latency {
+    ($c:expr, $max:expr, $measure:expr) => {
+        let mut group = $c.benchmark_group(function_name!());
+        for pct in &[10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110] {
+            group.bench_with_input(BenchmarkId::from_parameter(pct), &$max, |b, &max| {
+                b.iter_custom(|iters| {
+                    let mut backing_set: HashMap<usize, usize> = HashMap::with_capacity(max);
+                    (0..$max).for_each(|i| {
+                        backing_set.insert(i, i);
+                    });
+                    let data = run_single_thread_test(
+                        iters,
+                        iters / 10,
+                        *pct,
+                        backing_set,
+                        Some(Duration::from_nanos(5)),
+                        AccessPattern::Random(0, max),
+                    );
+                    println!("{:?}", data);
+                    data.elapsed
+                })
+            });
+        }
+        group.finish();
+    };
+}
+
+macro_rules! basic_single_thread_x_small_pct {
+    ($c:expr, $max:expr, $measure:expr) => {
+        let mut group = $c.benchmark_group(function_name!());
+        for pct in &[10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110] {
+            group.bench_with_input(BenchmarkId::from_parameter(pct), &$max, |b, &max| {
+                b.iter_custom(|iters| {
+                    let mut backing_set: HashMap<usize, usize> = HashMap::with_capacity(max);
+                    (0..$max).for_each(|i| {
+                        backing_set.insert(i, i);
+                    });
+                    let data = run_single_thread_test(
+                        iters,
+                        iters / 10,
+                        *pct,
+                        backing_set,
+                        Some(Duration::from_nanos(5)),
+                        AccessPattern::Random(0, max),
+                    );
+                    println!("{:?}", data);
+                    (data.hit_count, data.attempt)
+                })
+            });
+        }
+        group.finish();
+    };
+}
+
 #[named]
-pub fn basic_single_thread_2048_small(c: &mut Criterion) {
-    let max = 2048;
-    let mut group = c.benchmark_group(function_name!());
-    for pct in &[5, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110] {
-        group.bench_with_input(BenchmarkId::from_parameter(pct), &max, |b, &max| {
-            b.iter_custom(|iters| {
-                let mut backing_set: HashMap<usize, usize> = HashMap::with_capacity(max);
-                (0..max).for_each(|i| {
-                    backing_set.insert(i, i);
-                });
-                let data = run_single_thread_test(
-                    iters,
-                    iters / 10,
-                    *pct,
-                    backing_set,
-                    None,
-                    AccessPattern::Random(0, max),
-                );
-                println!("{:?}", data);
-                data.elapsed
-            })
-        });
-    }
-    group.finish();
+pub fn basic_single_thread_2048_small_latency(c: &mut Criterion) {
+    basic_single_thread_x_small_latency!(c, 2048, MeasureType::Latency);
+}
+
+#[named]
+pub fn basic_single_thread_2048_small_pct(c: &mut Criterion<HitPercentage>) {
+    basic_single_thread_x_small_pct!(c, 2048, MeasureType::HitPct);
 }
 
 criterion_group!(
-    name = basic;
+    name = latency;
     config = Criterion::default()
         // .measurement_time(Duration::from_secs(15))
         .with_plots();
-    targets = basic_single_thread_2048_small
+    targets = basic_single_thread_2048_small_latency
 );
 
-criterion_main!(basic);
+criterion_group!(
+    name = hit_percent;
+    config = Criterion::default().with_measurement(HitPercentage);
+    targets = basic_single_thread_2048_small_pct
+);
+
+criterion_main!(latency, hit_percent);
